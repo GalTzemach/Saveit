080af1da15105f46fe4a6a345741371b7d75f709
Tal <talzemah1@gmail.com>
2017-04-14 18:45:27 +0300

    ui / ux improve

--- app/src/main/java/com/example/galtzemach/saveit/BL/PhotosAdapter.java
+++ app/src/main/java/com/example/galtzemach/saveit/BL/PhotosAdapter.java
@@ -1,6 +1,8 @@
 package com.example.galtzemach.saveit.BL;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
@@ -28,7 +30,7 @@ public class PhotosAdapter extends ArrayAdapter<String>{
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View rowView = context.getLayoutInflater().inflate(R.layout.photos_view, null, true);
 
         TextView numImageTextView = (TextView) rowView.findViewById(R.id.number_image_view);
@@ -36,6 +38,19 @@ public class PhotosAdapter extends ArrayAdapter<String>{
 
         ImageView photoImageView = (ImageView) rowView.findViewById(R.id.image_view);
         Picasso.with(context.getApplicationContext()).load(photosArrayList.get(position)).into(photoImageView);
+
+        photoImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+//                getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(photosArrayList.get(position))));
+                getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(photosArrayList.get(position))));
+
+
+
+            }
+        });
 
         return rowView;
     }
